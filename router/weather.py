0ea002b18097d9754309f7f2d7121413fea3568f
sreeragunandha <sreeragunandha.t@akaiketech.com>
2024-11-27 18:32:14 +0530

    Add weather prediction endpoint and model integration

--- router/weather.py
+++ router/weather.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter,Query
 from utils.weatherApi import WeatherApiWrapperForTommorowIo,WeatherApiWrapper
 from model.pydantic_model import Location
+from pydantic import BaseModel
+import joblib
 router = APIRouter(
     prefix="/weather",
     tags=["weather"],
@@ -27,5 +29,40 @@ async def get_weather(
     try: 
         weather_data = weather1.get_weather_location(latitude,longitude)
         return weather_data
+    except Exception as e:
+        return {"error": str(e)}
+class WeatherDataRequest(BaseModel):
+    X: float
+    Y: float
+    Slope: float
+    Curvature: float
+    Aspect: float
+    TWI: float
+    FA: float
+    Drainage: float
+    Rainfall: float
+
+@router.post("/predict")
+async def post_weather_data(data: WeatherDataRequest):
+    try:
+        import os
+
+        # Load the model from the ml_model folder
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        print(current_dir)
+        model = joblib.load(os.path.join(f"{current_dir}/random_forest_model.pkl"))
+        encoder = joblib.load(os.path.join(f"{current_dir}/ordinal_encoder.pkl"))
+        # Prepare the data for prediction
+        input_data = [[
+            data.X, data.Y, data.Slope, data.Curvature, data.Aspect,
+            data.TWI, data.FA, data.Drainage, data.Rainfall
+        ]]
+        
+        print("hi")
+        prediction = model.predict(input_data)
+        print(prediction)
+        
+        label = encoder.inverse_transform([prediction])[0][0]
+        return label
     except Exception as e:
         return {"error": str(e)}
